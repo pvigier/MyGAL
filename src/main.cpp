4da48e0913fa36aca531bff907b2de54bcf8896f
Pierre Vigier <pierre.vigier@ymail.com>
2018-11-21 22:20:38 +0100

    Remove the SFML dependency

--- src/main.cpp
+++ src/main.cpp
@@ -20,15 +20,8 @@
 #include <vector>
 #include <chrono>
 #include <random>
-// SFML
-#include <SFML/Graphics.hpp>
 // My includes
 #include "FortuneAlgorithm.h"
-
-constexpr float WINDOW_WIDTH = 600.0f;
-constexpr float WINDOW_HEIGHT = 600.0f;
-constexpr float POINT_RADIUS = 0.005f;
-constexpr float OFFSET = 1.0f;
 
 std::vector<Vector2> generatePoints(int nbPoints)
 {
@@ -42,62 +35,6 @@ std::vector<Vector2> generatePoints(int nbPoints)
         points.push_back(Vector2{distribution(generator), distribution(generator)});
 
     return points;
-}
-
-void drawPoint(sf::RenderWindow& window, Vector2 point, sf::Color color)
-{
-    sf::CircleShape shape(POINT_RADIUS);
-    shape.setPosition(sf::Vector2f(point.x - POINT_RADIUS, 1 - point.y - POINT_RADIUS));
-    shape.setFillColor(color);
-    window.draw(shape);
-}
-
-void drawEdge(sf::RenderWindow& window, Vector2 origin, Vector2 destination, sf::Color color)
-{
-    sf::Vertex line[] =
-    {
-        sf::Vertex(sf::Vector2f(origin.x, 1.0f - origin.y), color),
-        sf::Vertex(sf::Vector2f(destination.x, 1.0f - destination.y), color)
-    };
-    window.draw(line, 2, sf::Lines);
-}
-
-void drawPoints(sf::RenderWindow& window, std::vector<Vector2> points)
-{
-    for (Vector2& point : points)
-        drawPoint(window, point, sf::Color(100, 250, 50));
-}
-
-void drawDiagram(sf::RenderWindow& window, VoronoiDiagram& diagram, int nbSites)
-{
-    for (int i = 0; i < nbSites; ++i)
-    {
-        const VoronoiDiagram::Site* site = diagram.getSite(i);
-        Vector2 center = site->point;
-        VoronoiDiagram::Face* face = site->face;
-        VoronoiDiagram::HalfEdge* halfEdge = face->outerComponent;
-        if (halfEdge == nullptr)
-            continue;
-        while (halfEdge->prev != nullptr)
-        {
-            halfEdge = halfEdge->prev;
-            if (halfEdge == face->outerComponent)
-                break;
-        }
-        VoronoiDiagram::HalfEdge* start = halfEdge;
-        while (halfEdge != nullptr)
-        {
-            if (halfEdge->origin != nullptr && halfEdge->destination != nullptr)
-            {
-                Vector2 origin = (halfEdge->origin->point - center) * OFFSET + center;
-                Vector2 destination = (halfEdge->destination->point - center) * OFFSET + center;
-                drawEdge(window, origin, destination, sf::Color::Red);
-            }
-            halfEdge = halfEdge->next;
-            if (halfEdge == start)
-                break;
-        }
-    }
 }
 
 int main()
@@ -124,29 +61,6 @@ int main()
     diagram.intersect(Box{0.0, 0.0, 1.0, 1.0});
     duration = std::chrono::steady_clock::now() - start;
     std::cout << "intersection: " << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() << "ms" << '\n';
-
-    // Display the diagram
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = 8;
-    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Fortune's algorithm", sf::Style::Default, settings);
-    window.setView(sf::View(sf::FloatRect(-0.1f, -0.1f, 1.2f, 1.2f)));
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear(sf::Color::Black);
-
-        drawDiagram(window, diagram, points.size());
-        drawPoints(window, points);
-
-        window.display();
-    }
 
     return 0;
 }
