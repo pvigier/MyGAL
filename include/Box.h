524c8211e5a68bead1d12723a5b326af49933991
Pierre Vigier <pierre.vigier@ymail.com>
2018-11-28 23:53:27 +0100

    Rewrite Box

--- include/Box.h
+++ include/Box.h
@@ -1,4 +1,4 @@
-/* MyGAL
+/* FortuneAlgorithm
  * Copyright (C) 2018 Pierre Vigier
  *
  * This program is free software: you can redistribute it and/or modify
@@ -19,9 +19,11 @@
 
 // STL
 #include <array>
+#include <limits>
 // My includes
 #include "Vector2.h"
 
+template<typename T>
 class Box
 {
 public:
@@ -31,16 +33,121 @@ public:
     struct Intersection
     {
         Side side;
-        Vector2 point;
+        Vector2<T> point;
     };
 
-    double left;
-    double bottom;
-    double right;
-    double top;
+    T left;
+    T bottom;
+    T right;
+    T top;
 
-    bool contains(const Vector2& point) const;
-    Intersection getFirstIntersection(const Vector2& origin, const Vector2& direction) const; // Useful for Fortune's algorithm
-    int getIntersections(const Vector2& origin, const Vector2& destination, std::array<Intersection, 2>& intersections) const; // Useful for diagram intersection
+    bool contains(const Vector2<T>& point) const
+    {
+        return point.x >= left - EPSILON && point.x <= right + EPSILON &&
+            point.y >= bottom  - EPSILON && point.y <= top + EPSILON;
+    }
+
+    // Useful for Fortune's algorithm
+    Intersection getFirstIntersection(const Vector2<T>& origin, const Vector2<T>& direction) const
+    {
+        // origin must be in the box
+        Intersection intersection;
+        T t = std::numeric_limits<T>::infinity();
+        if (direction.x > static_cast<T>(0.0))
+        {
+            t = (right - origin.x) / direction.x;
+            intersection.side = Side::RIGHT;
+            intersection.point = origin + t * direction;
+        }
+        else if (direction.x < static_cast<T>(0.0))
+        {
+            t = (left - origin.x) / direction.x;
+            intersection.side = Side::LEFT;
+            intersection.point = origin + t * direction;
+        }
+        if (direction.y > static_cast<T>(0.0))
+        {
+            T newT = (top - origin.y) / direction.y;
+            if (newT < t)
+            {
+                intersection.side = Side::TOP;
+                intersection.point = origin + newT * direction;
+            }
+        }
+        else if (direction.y < static_cast<T>(0.0))
+        {
+            T newT = (bottom - origin.y) / direction.y;
+            if (newT < t)
+            {
+                intersection.side = Side::BOTTOM;
+                intersection.point = origin + newT * direction;
+            }
+        }
+        return intersection;
+    }
+
+    // Useful for diagram intersection
+    int getIntersections(const Vector2<T>& origin, const Vector2<T>& destination, std::array<Intersection, 2>& intersections) const
+    {
+        // WARNING: If the intersection is a corner, both intersections are equals
+        Vector2 direction = destination - origin;
+        std::array<T, 2> t;
+        std::size_t i = 0; // index of the current intersection
+        // Left
+        if (origin.x < left - EPSILON || destination.x < left - EPSILON)
+        {   
+            t[i] = (left - origin.x) / direction.x;
+            if (t[i] > EPSILON && t[i] < static_cast<T>(1.0) - EPSILON)  
+            {
+                intersections[i].side = Side::LEFT;
+                intersections[i].point = origin + t[i] * direction;
+                if (intersections[i].point.y >= bottom  - EPSILON && intersections[i].point.y <= top + EPSILON)
+                    ++i;
+            }
+        }
+        // Right
+        if (origin.x > right + EPSILON || destination.x > right + EPSILON)
+        {   
+            t[i] = (right - origin.x) / direction.x;
+            if (t[i] > EPSILON && t[i] < static_cast<T>(1.0) - EPSILON)  
+            {
+                intersections[i].side = Side::RIGHT;
+                intersections[i].point = origin + t[i] * direction;
+                if (intersections[i].point.y >= bottom - EPSILON && intersections[i].point.y <= top + EPSILON)
+                    ++i;
+            }
+        }
+        // Bottom
+        if (origin.y < bottom - EPSILON || destination.y < bottom - EPSILON)
+        {   
+            t[i] = (bottom - origin.y) / direction.y;
+            if (i < 2 && t[i] > EPSILON && t[i] < static_cast<T>(1.0) - EPSILON)  
+            {
+                intersections[i].side = Side::BOTTOM;
+                intersections[i].point = origin + t[i] * direction;
+                if (intersections[i].point.x >= left  - EPSILON && intersections[i].point.x <= right + EPSILON)
+                    ++i;
+            }
+        }
+        // Top
+        if (origin.y > top + EPSILON || destination.y > top + EPSILON)
+        {   
+            t[i] = (top - origin.y) / direction.y;
+            if (i < 2 && t[i] > EPSILON && t[i] < static_cast<T>(1.0) - EPSILON)  
+            {
+                intersections[i].side = Side::TOP;
+                intersections[i].point = origin + t[i] * direction;
+                if (intersections[i].point.x >= left - EPSILON && intersections[i].point.x <= right + EPSILON)
+                    ++i;
+            }
+        }
+        // Sort the intersections from the nearest to the farthest
+        if (i == 2 && t[0] > t[1])
+            std::swap(intersections[0], intersections[1]);
+        return i;
+    }
+
+private:
+    static constexpr T EPSILON = std::numeric_limits<T>::epsilon();
 };
 
