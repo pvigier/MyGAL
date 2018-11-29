c1804c3343248d9e0f625cb3b3ff61c522214da6
Pierre Vigier <pierre.vigier@ymail.com>
2018-11-29 23:52:04 +0100

    Add example

---
+++ examples/main.cpp
@@ -0,0 +1,172 @@
+/* FortuneAlgorithm
+ * Copyright (C) 2018 Pierre Vigier
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// STL
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <random>
+// SFML
+#include <SFML/Graphics.hpp>
+// My includes
+#include "../include/FortuneAlgorithm.h"
+
+using Float = double;
+
+constexpr Float WINDOW_WIDTH = 600.0f;
+constexpr Float WINDOW_HEIGHT = 600.0f;
+constexpr Float POINT_RADIUS = 0.005f;
+constexpr Float OFFSET = 1.0f;
+
+template<typename T>
+std::vector<Vector2<T>> generatePoints(int nbPoints)
+{
+    auto seed = std::chrono::system_clock::now().time_since_epoch().count();
+    std::cout << "seed: " << seed << '\n';
+    auto generator = std::default_random_engine(seed);
+    auto distribution = std::uniform_real_distribution<T>(0.0, 1.0);
+
+    auto points = std::vector<Vector2<T>>(nbPoints);
+    for (auto i = 0; i < nbPoints; ++i)
+        points[i] = Vector2<T>(distribution(generator), distribution(generator));
+
+    return points;
+}
+
+template<typename T>
+void drawPoint(sf::RenderWindow& window, Vector2<T> point, sf::Color color)
+{
+    auto shape = sf::CircleShape(POINT_RADIUS);
+    shape.setPosition(sf::Vector2f(point.x - POINT_RADIUS, 1 - point.y - POINT_RADIUS));
+    shape.setFillColor(color);
+    window.draw(shape);
+}
+
+template<typename T>
+void drawEdge(sf::RenderWindow& window, Vector2<T> origin, Vector2<T> destination, sf::Color color)
+{
+    sf::Vertex line[] =
+    {
+        sf::Vertex(sf::Vector2f(origin.x, 1.0f - origin.y), color),
+        sf::Vertex(sf::Vector2f(destination.x, 1.0f - destination.y), color)
+    };
+    window.draw(line, 2, sf::Lines);
+}
+
+template<typename T>
+void drawPoints(sf::RenderWindow& window, VoronoiDiagram<T>& diagram)
+{
+    for (std::size_t i = 0; i < diagram.getNbSites(); ++i)
+        drawPoint(window, diagram.getSite(i)->point, sf::Color(100, 250, 50));
+}
+
+template<typename T>
+void drawDiagram(sf::RenderWindow& window, VoronoiDiagram<T>& diagram)
+{
+    for (std::size_t i = 0; i < diagram.getNbSites(); ++i)
+    {
+        auto site = diagram.getSite(i);
+        auto center = site->point;
+        auto face = site->face;
+        auto halfEdge = face->outerComponent;
+        if (halfEdge == nullptr)
+            continue;
+        while (halfEdge->prev != nullptr)
+        {
+            halfEdge = halfEdge->prev;
+            if (halfEdge == face->outerComponent)
+                break;
+        }
+        auto start = halfEdge;
+        while (halfEdge != nullptr)
+        {
+            if (halfEdge->origin != nullptr && halfEdge->destination != nullptr)
+            {
+                auto origin = (halfEdge->origin->point - center) * OFFSET + center;
+                auto destination = (halfEdge->destination->point - center) * OFFSET + center;
+                drawEdge(window, origin, destination, sf::Color::Red);
+            }
+            halfEdge = halfEdge->next;
+            if (halfEdge == start)
+                break;
+        }
+    }
+}
+
+template<typename T>
+VoronoiDiagram<T> generateRandomDiagram(std::size_t nbPoints)
+{
+    // Generate points
+    auto points = generatePoints<T>(nbPoints);
+
+    // Construct diagram
+    auto algorithm = FortuneAlgorithm<T>(points);
+    auto start = std::chrono::steady_clock::now();
+    algorithm.construct();
+    auto duration = std::chrono::steady_clock::now() - start;
+    std::cout << "construction: " << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() << "ms" << '\n';
+
+    // Bound the diagram
+    start = std::chrono::steady_clock::now();
+    algorithm.bound(Box<T>{-0.05, -0.05, 1.05, 1.05}); // Take the bounding box slightly bigger than the intersection box
+    duration = std::chrono::steady_clock::now() - start;
+    std::cout << "bounding: " << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() << "ms" << '\n';
+    auto diagram = algorithm.getDiagram();
+
+    // Intersect the diagram with a box
+    start = std::chrono::steady_clock::now();
+    bool valid = diagram.intersect(Box<T>{0.0, 0.0, 1.0, 1.0});
+    duration = std::chrono::steady_clock::now() - start;
+    std::cout << "intersection: " << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() << "ms" << '\n';
+    if (!valid)
+        throw std::runtime_error("An error occured in the box intersection algorithm");
+
+    return diagram;
+}
+
+int main()
+{
+    auto nbPoints = 100;
+    auto diagram = generateRandomDiagram<Float>(nbPoints);
+
+    // Display the diagram
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Fortune's algorithm", sf::Style::Default, settings);
+    window.setView(sf::View(sf::FloatRect(-0.1f, -0.1f, 1.2f, 1.2f)));
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else if (event.type == sf::Event::KeyReleased && event.key.code == sf::Keyboard::Key::N)
+                diagram = generateRandomDiagram<Float>(nbPoints);
+        }
+
+        window.clear(sf::Color::Black);
+
+        drawDiagram(window, diagram);
+        drawPoints(window, diagram);
+
+        window.display();
+    }
+
+    return 0;
+}
