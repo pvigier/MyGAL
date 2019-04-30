416ee83919d557d28287f0689bc1c255059030f5
Pierre Vigier <pierre.vigier@ymail.com>
2019-04-30 18:03:58 +0200

    Update or add the license preambles

--- include/MyGAL/Diagram.h
+++ include/MyGAL/Diagram.h
@@ -1,5 +1,5 @@
 /* MyGAL
- * Copyright (C) 2018 Pierre Vigier
+ * Copyright (C) 2019 Pierre Vigier
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
