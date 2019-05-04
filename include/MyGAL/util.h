fba61c7bbb751a718edb9be6748e850c9d448b2e
Pierre Vigier <pierre.vigier@ymail.com>
2019-05-04 16:24:27 +0200

    Make the simple functions from util constexpr and noexcept

--- include/MyGAL/util.h
+++ include/MyGAL/util.h
@@ -23,36 +23,36 @@ namespace mygal
 {
 
 template<typename T>
-static constexpr T EPSILON = std::numeric_limits<T>::epsilon();
+constexpr T EPSILON = std::numeric_limits<T>::epsilon();
 
 // Almost predicates are easier to satisfy than the normal ones
 
 template<typename T>
-bool almostLower(T lhs, T rhs)
+constexpr bool almostLower(T lhs, T rhs) noexcept
 {
     return lhs <= rhs + EPSILON<T>;
 }
 
 template<typename T>
-bool almostGreater(T lhs, T rhs)
+constexpr bool almostGreater(T lhs, T rhs) noexcept
 {
     return lhs >= rhs - EPSILON<T>;
 }
 
 template<typename T>
-bool almostEqual(T lhs, T rhs)
+constexpr bool almostEqual(T lhs, T rhs) noexcept
 {
     return almostLower(lhs, rhs) && almostGreater(lhs, rhs);
 }
 
 template<typename T>
-bool almostZero(T x)
+constexpr bool almostZero(T x) noexcept
 {
     return almostEqual(x, static_cast<T>(0));
 }
 
 template<typename T>
-bool almostBetween(T x, T a, T b)
+constexpr bool almostBetween(T x, T a, T b) noexcept
 {
     return almostGreater(x, a) && almostLower(x, b);
 }
@@ -60,19 +60,19 @@ bool almostBetween(T x, T a, T b)
 // Strictly predicates are harder to satisfy than the normal ones
 
 template<typename T>
-bool strictlyLower(T lhs, T rhs)
+constexpr bool strictlyLower(T lhs, T rhs) noexcept
 {
     return lhs < rhs - EPSILON<T>;
 }
 
 template<typename T>
-bool strictlyGreater(T lhs, T rhs)
+constexpr bool strictlyGreater(T lhs, T rhs) noexcept
 {
     return lhs > rhs + EPSILON<T>;
 }
 
 template<typename T>
-bool strictlyBetween(T x, T a, T b)
+constexpr bool strictlyBetween(T x, T a, T b) noexcept
 {
     return strictlyGreater(x, a) && strictlyLower(x, b);
 }
